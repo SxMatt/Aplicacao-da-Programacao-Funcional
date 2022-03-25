2b22d7edcde456fd24f2dce2b86589b3757e3085
Mateus Santos <65631460+SxMatt@users.noreply.github.com>
2022-03-25 09:25:41 -0300

    Update conversorFahrenheintKelvin.js

--- conversorFahrenheintKelvin.js
+++ conversorFahrenheintKelvin.js
@@ -1,6 +1,6 @@
-var fahrenheit = [250, 300, 257, 280, 430, 500];
+const fahrenheit = [250, 300, 257, 280, 430, 500];
 
-var kelvin = fahrenheit.map(function(elem){
+const kelvin = fahrenheit.map(function(elem){
     return Math.round(((elem - 273.15) * (9/5) + 32));
   }
  );
